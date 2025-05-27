89f28f4bce5ac3124817c39b07fed459ad2dec65
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-27 16:23:38 +0300

    Add input type

---
+++ src/app/Shared/components/input-field/types/inputTypes.type.ts
@@ -0,0 +1,6 @@
+export type InputType = 'text' | 'email' | 'password' | 'number' | 'tel' | 'url';
+export type InputSize = 'sm' | 'md' | 'lg';
+export type InputState = 'default' | 'valid' | 'invalid' | 'loading' | 'disabled';
+export type InputShape = 'rounded' | 'square' | 'pill';
+export type InputIconPosition = 'left' | 'right';
+export type InputIconType = 'search' | 'user' | 'lock' | 'eye' | 'eye-off' | 'calendar' | 'phone' | 'mail' | 'check' | 'cross';
