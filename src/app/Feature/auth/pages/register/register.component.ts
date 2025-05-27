a51f1af59e0f22903a422018177dd3415b23f178
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-27 16:33:11 +0300

    Add register component

--- src/app/Feature/auth/pages/register/register.component.ts
+++ src/app/Feature/auth/pages/register/register.component.ts
@@ -1,11 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { InputFieldComponent } from '../../../../Shared/components/input-field/input-field.component';
+import { ButtonComponent } from '../../../../Shared/components/button/button.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+    InputFieldComponent,
+    ButtonComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  isLoading = false;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      agreeTerms: [false, [Validators.requiredTrue]]
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
+
+  passwordMatchValidator(form: FormGroup): { [key: string]: boolean } | null {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+
+    if (!password || !confirmPassword) {
+      return null;
+    }
+
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ ...confirmPassword.errors, 'custom': 'Passwords do not match' });
+      return { passwordsDoNotMatch: true };
+    }
+
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+  }
 }
