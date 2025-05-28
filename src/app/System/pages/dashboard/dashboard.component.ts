4f5309a965f7ec3decb475b840078b4aa8ae3e90
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-29 00:01:49 +0300

    Add table

--- src/app/System/pages/dashboard/dashboard.component.ts
+++ src/app/System/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+// dashboard.component.ts
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CurrencyPipe } from '@angular/common';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CurrencyPipe , TableModule  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  products: any[] = [];
+  selectedProducts: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('https://dummyjson.com/products?limit=12').subscribe((res) => {
+      this.products = res.products;
+    });
+  }
+
+  exportCSV(table: any) {
+    table.exportCSV();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { providePrimeNG } from 'primeng/config'; 
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './Core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withInterceptors([authInterceptor])) , provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideHttpClient(withInterceptors([authInterceptor])) , provideAnimationsAsync(), providePrimeNG({ theme: { preset: Aura } }) , provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+
+
+
