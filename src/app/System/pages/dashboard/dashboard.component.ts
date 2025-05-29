71a720bd045336915d5571a7294ed527da9b5922
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-29 12:28:52 +0300

    add table Filter

--- src/app/System/pages/dashboard/dashboard.component.ts
+++ src/app/System/pages/dashboard/dashboard.component.ts
@@ -1,28 +1,50 @@
 // dashboard.component.ts
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { TagModule } from 'primeng/tag';
+import { IconFieldModule } from 'primeng/iconfield';
+import { ButtonModule } from 'primeng/button';
+import { InputIconModule } from 'primeng/inputicon';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CurrencyPipe , TableModule  ],
+  imports: [TableModule, CommonModule, InputTextModule, TagModule,
+    SelectModule, MultiSelectModule, ButtonModule, IconFieldModule, InputIconModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   products: any[] = [];
   selectedProducts: any[] = [];
+  exportColumns!: any[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get<any>('https://dummyjson.com/products?limit=12').subscribe((res) => {
       this.products = res.products;
+      this.exportColumns = this.products.map((product: any) => {
+        return { title: product.name, dataKey: product.id };
+      });
     });
   }
 
+  onFilter(event: Event, field: string, table: any) {
+  const input = event.target as HTMLInputElement;
+  table.filter(input.value, field, 'contains');
+}
+  onFilterGlobal(event: Event, table: any) {
+  const input = event.target as HTMLInputElement;
+  table.filterGlobal(input.value,  'contains');
+}
   exportCSV(table: any) {
+    const data = Array.isArray(table.filteredValue) ? table.filteredValue : table.value;
+    table.filteredValue = data;
     table.exportCSV();
   }
 }
